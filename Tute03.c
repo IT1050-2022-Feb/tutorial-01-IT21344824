9d69d3e2b5ebc344caf3767638e55d673448dfa5
AKIDU <99312096+IT21344824@users.noreply.github.com>
2022-02-09 06:24:18 +0000

    done 3

--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,16 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int x , sum = 0 , i ;
+
+  printf("enter the number :");
+  scanf("%d" , &x);
+
+  for ( i=1 ; i<=x ; i++)
+  {
+    sum = sum + i ;
+  }
+  printf("sum : %d" , sum);
   return 0;
 }
 
