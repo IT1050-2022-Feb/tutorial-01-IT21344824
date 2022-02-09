edfd0ff49cf928b309a28492efb225c6f7ce15d5
AKIDU <99312096+IT21344824@users.noreply.github.com>
2022-02-09 06:04:33 +0000

    done

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,18 @@
 
 int main() {
   
+  int m1 ,m2 ;
+  float avg ;
+
+  printf("enter mark 1 :");
+  scanf("%d" , &m1);
+
+  printf("enter mark 2 :");
+  scanf("%d" , &m2);
+
+  avg = (float)(m1 + m2 )/2 ;
+
+  printf("%.2f" , avg );
   return 0;
 }
 
